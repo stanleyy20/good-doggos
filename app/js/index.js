1282ee863b58382ec0d393271391851bc5647fa9
Dawid Stach <dstach20@gmail.com>
2022-06-30 12:36:21 +0200

    create class Dogo and basic method

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,36 @@
-import './index.scss';
+import '../sass/index.scss';
 
-console.log('Boilerplate is working!');
+class Dogog {
+  constructor() {
+    this.apiUrl = 'https://dog.ceo/api';
+    this.imgEl = document.querySelector('.image-dog');
+
+    this.init();
+  }
+
+  listBreeds = () => {
+    return fetch(`${this.apiUrl}/breeds/list/all`)
+      .then((resp) => resp.json())
+      .then((data) => data.message);
+  };
+
+  getRandomImage = () => {
+    return fetch(`${this.apiUrl}/breeds/image/random`)
+      .then((resp) => resp.json())
+      .then((data) => data.message);
+  };
+
+  getRandomImageByBreed = (breed) => {
+    return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+      .then((resp) => resp.json())
+      .then((data) => data.message);
+  };
+
+  init = () => {
+    this.getRandomImage().then((src) => this.imgEl.setAttribute('src', src));
+  };
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Dogog();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 const path = require('path');
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const env = process.env.NODE_ENV;
 
 module.exports = {
-  entry: './app/index.js',
+  entry: './app/js/index.js',
 
   mode: env,
 
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    publicPath: '/'
+    publicPath: '/',
   },
 
   devServer: {
@@ -21,7 +21,7 @@ module.exports = {
       {
         directory: path.join(__dirname, 'public'),
         watch: true,
-      }
+      },
     ],
     port: 3500,
   },
@@ -31,27 +31,23 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: 'babel-loader',
       },
       {
         test: /\.(sa|sc|c)ss$/,
-        use: [
-          env == 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ]
-      }
-    ]
+        use: [env == 'development' ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
   },
 
   plugins: [
-    new HtmlWebpackPlugin({template: './app/index.html'}),
+    new HtmlWebpackPlugin({ template: './app/index.html' }),
     new webpack.HotModuleReplacementPlugin(),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 };
